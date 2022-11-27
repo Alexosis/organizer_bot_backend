3421cb1202c8a14ce6fccc76bdb7cca1cbe53bbd
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 12:39:30 +0300

    fixed: send data

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ const bot = new TelegramBot(token, {polling: true});
 bot.on('message', async(msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
+    const userID = msg?.from?.id
+
 
     if(text == '/start'){
         await bot.sendMessage(chatId, 'Ниже кнопка, заполни форму',{
             reply_markup:{
-                keyboard:[
+                inline_keyboard:[
                     [{text: 'добавить событие', web_app:{url:webAppUrl + '/form'}}],
                 ]
             }
@@ -22,14 +24,17 @@ bot.on('message', async(msg) => {
     }
 
     if(msg?.web_app_data?.data){
-
+        let raw = JSON.parse(msg?.web_app_data?.data);
+        raw["todos"].userID=userID;
+        const data = raw;
+        console.log(data);
         try{
             fetch('http://127.0.0.1:8000/api/todos/post', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: msg?.web_app_data?.data
+                body: JSON.stringify(data)
             })
               .then(() => {
                   const data = JSON.parse(msg?.web_app_data?.data);
